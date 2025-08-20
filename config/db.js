f4afd1a41d3109d449beae7797f717f64a47af54
Akshay Kumar <72611036+Akshay20186@users.noreply.github.com>
2025-08-20 13:10:38 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
-const mysql = require('mysql2/promise'); // since you're using PromisePool
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('DB connection failed:', err);
+  } else {
+    console.log('DB connected successfully');
+  }
 });
