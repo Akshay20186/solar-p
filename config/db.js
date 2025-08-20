36fac2cf4be07bc32fe3836b88a2c7648ce434b3
Akshay Kumar <72611036+Akshay20186@users.noreply.github.com>
2025-08-21 00:01:45 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-require('dotenv').config(); const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql2');
 
 // create a connection
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-// test the connection
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting:', err.message);
-    return;
-  }
-  console.log('Connected to MySQL!');
+db.connect(err => {
+  if (err) throw err;
+  console.log("MySQL Connected...");
 });
--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
 });
